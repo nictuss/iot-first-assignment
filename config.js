349dcbd22ac6f50edf9c68826f7d14b066ff88e2
nictuss <nicolo.palmiero@gmail.com>
2020-03-25 15:57:38 +0100

    modified config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.endpoint = 'https://iotpersistencelayer.documents.azure.com:443/'
-config.key = 'vRaX4w2zuNNgKZUnmpem8CRUMeQSZTjnnUMhBv0TKWVOuEmv7jj703LMEfcmE3oCPuHYHfNKJoVI4bIxEByAsA=='
+config.endpoint = "~your Azure Cosmos DB endpoint uri here~";
+config.key = "~your primary key here~";
 
 config.database = {
   id: 'Meteo_telemetry'
@@ -10,80 +10,3 @@ config.database = {
 config.container = {
   id: 'devices_telemetry'
 }
-
-config.items = {
-  Andersen: {
-    id: 'Anderson.1',
-    Country: 'USA',
-    lastName: 'Andersen',
-    parents: [
-      {
-        firstName: 'Thomas'
-      },
-      {
-        firstName: 'Mary Kay'
-      }
-    ],
-    children: [
-      {
-        firstName: 'Henriette Thaulow',
-        gender: 'female',
-        grade: 5,
-        pets: [
-          {
-            givenName: 'Fluffy'
-          }
-        ]
-      }
-    ],
-    address: {
-      state: 'WA',
-      county: 'King',
-      city: 'Seattle'
-    }
-  },
-  Wakefield: {
-    id: 'Wakefield.7',
-    Country: 'Italy',
-    parents: [
-      {
-        familyName: 'Wakefield',
-        firstName: 'Robin'
-      },
-      {
-        familyName: 'Miller',
-        firstName: 'Ben'
-      }
-    ],
-    children: [
-      {
-        familyName: 'Merriam',
-        firstName: 'Jesse',
-        gender: 'female',
-        grade: 8,
-        pets: [
-          {
-            givenName: 'Goofy'
-          },
-          {
-            givenName: 'Shadow'
-          }
-        ]
-      },
-      {
-        familyName: 'Miller',
-        firstName: 'Lisa',
-        gender: 'female',
-        grade: 1
-      }
-    ],
-    address: {
-      state: 'NY',
-      county: 'Manhattan',
-      city: 'NY'
-    },
-    isRegistered: false
-  }
-}
-
-module.exports = config
